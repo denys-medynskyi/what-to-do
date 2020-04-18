25854e9134a1c0b975395322180fdd30e406f0f0
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-04-18 07:08:30 +0300

    Add generating activity every x mlseconds

--- js/index.js
+++ js/index.js
@@ -1,14 +1,29 @@
 // let's load activities before user click
 async function loadActivities () {
-  let response = await fetch('https://raw.githubusercontent.com/deny7ko/what-to-do/master/activities.txt')
+  let response = await fetch('https://raw.githubusercontent.com/deny7ko/what-to-do/master/activities.json')
   let data = await response.json()
-  debugger
 
-  return data.split("\n").filter(el => el != ""); // there is "" as the last item in the list
+  return data; // there is "" as the last item in the list
 }
 
-let activities = loadActivities()
-//
+loadActivities().then(activities => {
+  let onStartClick = startGenerating.bind(null, activities);
+  let generatingTriggerElement = document.getElementById('js-random-activity-trigger');
+
+  generatingTriggerElement.addEventListener('click', onStartClick);
+  // generatingTriggerElement.addEventListener('click', onStopClick);
+}).catch(reason => console.log(reason.message));
+
+function startGenerating (activities, event) {
+  event.preventDefault();
+
+  return setInterval(() => {
+    const activity = pickActivity(activities);
+    showActivity(activity)
+  }, 200)
+}
+
+
 
 function pickActivity (activities) {
   const randomActivityIndex = Math.floor(Math.random() * activities.length)
@@ -16,18 +31,11 @@ function pickActivity (activities) {
 }
 
 function showActivity (activity) {
-  document.getElementById('js-random-activity-container').innerHTML = activity;
+  // let element = createActivityElement(activity);
+  // debugger
+  const activityContainer = document.getElementById('js-random-activity-container')
+  activityContainer.innerHTML = activity.title;
+  activityContainer.style.color = randomColor();
 }
 
-function onActivityClick (event) {
-  event.preventDefault()
-
-  activities
-    .then(activities => {
-      const activity = pickActivity(activities)
-      showActivity(activity)
-    })
-    .catch(reason => console.log(reason.message))
-}
-
-document.getElementById('js-random-activity-trigger').addEventListener('click', onActivityClick)
+function randomColor () { return "#" + Math.floor(Math.random()*16777215).toString(16) }
