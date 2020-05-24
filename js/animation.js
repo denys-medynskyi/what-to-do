327fe185a415db589920c1a4bd54ae743dbcf74a
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-05-24 17:59:20 +0300

    Add initial position of animated elements

---
+++ js/animation.js
@@ -0,0 +1,11 @@
+window.onload = (event) => {
+  let ticking = false;
+
+  function triggerAnimation() {
+    // Do something with the scroll position
+  }
+
+  window.addEventListener('scroll', function(e) {
+    console.log('s1')
+  });
+}
