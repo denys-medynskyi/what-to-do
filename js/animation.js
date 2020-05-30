58c2fd6efe9560847b5aff0aded046ac6e3e311a
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-05-30 09:45:14 +0300

    Add animations

--- js/animation.js
+++ js/animation.js
@@ -2,37 +2,34 @@ window.onload = (event) => {
   // To animate element it should have
   // 1. class: is-invisible
   // 2. id: element-N. Elements are animated incrementalls from element-1 to element-N/
+  // 3. data-animation-type: Which kind of animation should be applied
   // HTML element example:
-  // <footer class="is-invisible" id="element-7">
-  var elementNumber = 2
+  // <footer class="is-invisible" id="element-7" data-animation-type='fade-in'>
+  var animationNumber = 1
   const ELEMENT_INVISIBLE_CLASS = 'is-invisible'
-  const ANIMATION_TRIGGER_ID = 'js-animation-trigger'
 
   function calculateElementAnimationStart (element) {
     return window.innerWidth - element.getBoundingClientRect().x
   }
 
-  function triggerElementAnimation (element, animationStartPosition) {
-    element.style.transform = "translateX(" + animationStartPosition + "px" + ")";
+  function triggerElementAnimation (element) {
+    const animationType = element.getAttribute('data-animation-type')
+
+    if(animationType == 'slide-in') {
+      element.style.transform = "translateX(" + calculateElementAnimationStart(element) + "px" + ")";
+    }
+
     showElement(element)
-    element.classList.add('slide-in')
+    element.classList.add(animationType)
   }
 
   function showElement (element) {
     element.classList.remove(ELEMENT_INVISIBLE_CLASS)
   }
 
-  function hideElement (element) {
-    element.classList.add(ELEMENT_INVISIBLE_CLASS)
-  }
-
   function skipAnimation () {
     Array.from(document.getElementsByClassName(ELEMENT_INVISIBLE_CLASS)).forEach(element => showElement(element))
     hideTrigger()
-  }
-
-  function hideTrigger() {
-    hideElement(document.getElementById(ANIMATION_TRIGGER_ID)) // hide trigger when nothing to animate
   }
 
   document.addEventListener('keyup', event => {
@@ -43,14 +40,16 @@ window.onload = (event) => {
 
     // next animation on Space
     if (event.code === 'Space') {
-      const element = document.getElementById('element-' + elementNumber);
-      if (element == null) {
-        hideTrigger()
-        console.error("element with #id=" + 'element-' + elementNumber + " " + "not found")
+      const query = `[data-animation-order='${animationNumber}']`
+      const elements = Array.from(document.querySelectorAll(query))
+
+      if (elements.length == 0) {
+        console.error("element with " + query + " " + "not found")
         return
       }
-      triggerElementAnimation(element, calculateElementAnimationStart(element))
-      elementNumber += 1; // take next element
+
+      elements.forEach(element => triggerElementAnimation(element))
+      animationNumber += 1; // take next element
     }
   })
 }
