b7cafa497f7ebdab43a3da806aa17dd02c00e828
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-05-30 05:50:47 +0300

    Add first animated element

--- js/animation.js
+++ js/animation.js
@@ -1,11 +1,25 @@
 window.onload = (event) => {
-  let ticking = false;
+  var elementNumber = 2
 
-  function triggerAnimation() {
-    // Do something with the scroll position
+  function calculateElementAnimationStart (element) {
+    return window.innerWidth - element.getBoundingClientRect().x
   }
 
-  window.addEventListener('scroll', function(e) {
-    console.log('s1')
-  });
+  function triggerelementAnimation (element, animationStartPosition) {
+    element.style.transform = "translateX(" + animationStartPosition + "px" + ")";
+    element.classList.remove('is-invisible')
+    element.classList.add('slide-in')
+  }
+
+  document.addEventListener('keyup', event => {
+    if (event.code === 'Space') {
+      const element = document.getElementById('element-' + elementNumber);
+      if (element == null) {
+        console.error("element with #id=" + 'element-' + elementNumber + " " + "not found")
+        return
+      }
+      triggerelementAnimation(element, calculateElementAnimationStart(element))
+      elementNumber += 1; // take next element
+    }
+  })
 }
